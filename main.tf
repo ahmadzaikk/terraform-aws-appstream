ea548a4f43e2e9ac129cb0c67703d3cad03b633f
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2024-06-28 12:17:39 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -62,20 +62,13 @@ resource "aws_appstream_stack" "this" {
   name         = join("-", [var.name, "stack"])
   display_name = join("-", [var.name, "stack"])
   description  = join("-", [var.name, "stack"])
-  dynamic "access_endpoints" {
-    for_each = var.enable_vpce ? [] : [1]
-    content {
-      endpoint_type = "STREAMING"
-    }
-  }
-
-  dynamic "access_endpoints" {
-    for_each = var.enable_vpce ? [1] : []
-    content {
-      endpoint_type = "STREAMING"
-      vpce_id       = aws_vpc_endpoint.appstream_vpce[0].id
-    }
-  }
+ # dynamic "access_endpoints" {
+  #  for_each = var.enable_vpce ? [1] : []
+   # content {
+    #  endpoint_type = "STREAMING"
+     # vpce_id       = aws_vpc_endpoint.appstream_vpce[0].id
+    #}
+  #}
 
  
 
