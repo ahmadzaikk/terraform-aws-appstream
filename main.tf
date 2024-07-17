97fd051266e0c893d309fd206fba4637ad2f58dc
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2024-07-17 13:18:29 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -61,9 +61,8 @@ resource "aws_appstream_stack" "this" {
   name         = join("-", [var.name, "stack"])
   display_name = join("-", [var.name, "stack"])
   description  = join("-", [var.name, "stack"])
-   storage_connectors {
+  storage_connectors {
     connector_type = "HOMEFOLDERS"
-    resource_identifier = "arn:aws:s3:::kk-test-appstream-user"
   }
 
   dynamic "user_settings" {
